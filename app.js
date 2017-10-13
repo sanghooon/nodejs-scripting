e534706a83088b32a1f3b435c5e777c45bbb915a
Sanghoon Han <sanghooon@yahoo.com>
2017-10-13 08:59:28 -0400

    JSONify
    JSONify the header and body data

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+http.createServer((request, response) => {
+  const { headers, method, url } = request;
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+    response.on('error', (err) => {
+      console.err(err);
+    });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // or response.writeHead(200, {'Content-Type', 'application/json'});
+
+    const responseBody = { headers, method, url, body };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+
+  });
+}).listen(3000, () => {
+  console.log('Server running on 3000...');
 });
